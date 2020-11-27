2ba41da9d011e07e2ae7be1fe9e24030e275d01e
asiljak1 <asiljak1@etf.unsa.ba>
2020-11-27 01:32:40 +0100

    napravljen modul i testovi za prvu metodu

--- spirala2rasporedi.js
+++ spirala2rasporedi.js
@@ -10,45 +10,45 @@ function init(){
     dodajAktivnost(okvir,"DM","predavanje",16,19,"Utorak");
     dodajAktivnost(okvir,"OI","predavanje",12,15,"Srijeda");
 
-    //moj raspored sa validnim podacima
-    okvir=document.getElementById("okvir2");
-    iscrtajRaspored(okvir,["Ponedjeljak","Utorak","Srijeda","Četvrtak"],9,19);
-    dodajAktivnost(okvir,"WT","predavanje",9,12,"Srijeda");
-    dodajAktivnost(okvir,"WT","vježba",10.5,12,"Utorak");
-    dodajAktivnost(okvir,"PWS","predavanje",15,17,"Ponedjeljak");
-    dodajAktivnost(okvir,"PWS","vježba",17,19,"Ponedjeljak");
-    dodajAktivnost(okvir,"VVS","vježba",9,10.5,"Utorak");
-    dodajAktivnost(okvir,"OOI","predavanje",12,15,"Utorak");
-    dodajAktivnost(okvir,"OIS","predavanje",15,17,"Utorak");
-    dodajAktivnost(okvir,"OIS","vježba",17,19,"Utorak");
-    dodajAktivnost(okvir,"OOI","vježba",13,14,"Srijeda");
-    dodajAktivnost(okvir,"RG","vježba",14,16,"Srijeda");
-    dodajAktivnost(okvir,"RG","predavanje",9,11,"Četvrtak");
-    dodajAktivnost(okvir,"RG","vježba",11,12,"Četvrtak");
-    dodajAktivnost(okvir,"VVS","predavanje",12,15,"Četvrtak");
-    dodajAktivnost(okvir,"OOI","predavanje",15,16,"Četvrtak");
+    // //moj raspored sa validnim podacima
+    // okvir=document.getElementById("okvir2");
+    // iscrtajRaspored(okvir,["Ponedjeljak","Utorak","Srijeda","Četvrtak"],9,19);
+    // dodajAktivnost(okvir,"WT","predavanje",9,12,"Srijeda");
+    // dodajAktivnost(okvir,"WT","vježba",10.5,12,"Utorak");
+    // dodajAktivnost(okvir,"PWS","predavanje",15,17,"Ponedjeljak");
+    // dodajAktivnost(okvir,"PWS","vježba",17,19,"Ponedjeljak");
+    // dodajAktivnost(okvir,"VVS","vježba",9,10.5,"Utorak");
+    // dodajAktivnost(okvir,"OOI","predavanje",12,15,"Utorak");
+    // dodajAktivnost(okvir,"OIS","predavanje",15,17,"Utorak");
+    // dodajAktivnost(okvir,"OIS","vježba",17,19,"Utorak");
+    // dodajAktivnost(okvir,"OOI","vježba",13,14,"Srijeda");
+    // dodajAktivnost(okvir,"RG","vježba",14,16,"Srijeda");
+    // dodajAktivnost(okvir,"RG","predavanje",9,11,"Četvrtak");
+    // dodajAktivnost(okvir,"RG","vježba",11,12,"Četvrtak");
+    // dodajAktivnost(okvir,"VVS","predavanje",12,15,"Četvrtak");
+    // dodajAktivnost(okvir,"OOI","predavanje",15,16,"Četvrtak");
 
-    //slučajevi sa greškama pri pozivu funkcije iscrtajRaspored
-    okvir = document.getElementById("okvir3");
-    iscrtajRaspored(okvir,["Ponedjeljak","Utorak","Srijeda","Četvrtak","Petak"],-8,21);
-    iscrtajRaspored(okvir,["Ponedjeljak","Utorak","Srijeda","Četvrtak","Petak"],21,5);
-    iscrtajRaspored(okvir,["Ponedjeljak","Utorak","Srijeda","Četvrtak","Petak"],4,30);
+    // //slučajevi sa greškama pri pozivu funkcije iscrtajRaspored
+    // okvir = document.getElementById("okvir3");
+    // iscrtajRaspored(okvir,["Ponedjeljak","Utorak","Srijeda","Četvrtak","Petak"],-8,21);
+    // iscrtajRaspored(okvir,["Ponedjeljak","Utorak","Srijeda","Četvrtak","Petak"],21,5);
+    // iscrtajRaspored(okvir,["Ponedjeljak","Utorak","Srijeda","Četvrtak","Petak"],4,30);
 
-    //slučajevi sa greškama pri pozivu funkcije dodajAktivnost 
-    dodajAktivnost(okvir,"WT","predavanje",9,12,"Srijeda"); //slucaj sa nekreiranim rasporedom
-    dodajAktivnost(null,"WT","predavanje",9,12,"Srijeda"); 
+    // //slučajevi sa greškama pri pozivu funkcije dodajAktivnost 
+    // dodajAktivnost(okvir,"WT","predavanje",9,12,"Srijeda"); //slucaj sa nekreiranim rasporedom
+    // dodajAktivnost(null,"WT","predavanje",9,12,"Srijeda"); 
 
-    iscrtajRaspored(okvir,["Ponedjeljak","Utorak","Srijeda","Četvrtak"],9,24);
+    // iscrtajRaspored(okvir,["Ponedjeljak","Utorak","Srijeda","Četvrtak"],9,24);
 
-    dodajAktivnost(okvir,"WT","predavanje",-9,12,"Srijeda");
-    dodajAktivnost(okvir,"WT","vježba",10.2,12,"Utorak");
-    dodajAktivnost(okvir,"PWS","predavanje",17,14,"Ponedjeljak");
-    dodajAktivnost(okvir,"PWS","predavanje",17,26,"Ponedjeljak");
+    // dodajAktivnost(okvir,"WT","predavanje",-9,12,"Srijeda");
+    // dodajAktivnost(okvir,"WT","vježba",10.2,12,"Utorak");
+    // dodajAktivnost(okvir,"PWS","predavanje",17,14,"Ponedjeljak");
+    // dodajAktivnost(okvir,"PWS","predavanje",17,26,"Ponedjeljak");
 
 
-    dodajAktivnost(okvir,"PWS","vježba",17,19,"Ponedjeljak");
-    dodajAktivnost(okvir,"PWS","vježba",17,19,"Ponedjeljak");
-    dodajAktivnost(okvir,"PWS","vježba",17.5,19.5,"Ponedjeljak");
-    dodajAktivnost(okvir,"PWS","vježba",18,21,"Ponedjeljak");
-    dodajAktivnost(okvir,"PWS","vježba",16.5,19,"Ponedjeljak");
+    // dodajAktivnost(okvir,"PWS","vježba",17,19,"Ponedjeljak");
+    // dodajAktivnost(okvir,"PWS","vježba",17,19,"Ponedjeljak");
+    // dodajAktivnost(okvir,"PWS","vježba",17.5,19.5,"Ponedjeljak");
+    // dodajAktivnost(okvir,"PWS","vježba",18,21,"Ponedjeljak");
+    // dodajAktivnost(okvir,"PWS","vježba",16.5,19,"Ponedjeljak");
 }
