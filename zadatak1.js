dbf500542c50319c9d6ffcb1021e66c7356a9e95
asiljak1 <asiljak1@etf.unsa.ba>
2020-12-27 23:15:31 +0100

    Revert "zadatak 1"
    This reverts commit cd20cac221607a28a7f2280ee317338921420d65.

--- zadatak1.js
+++
@@ -1,8 +0,0 @@
-const express = require ( 'express' );
-const bodyParser = require ( 'body-parser' );
-const app = express ();
-
-app . use ( bodyParser . json ());
-app . use ( bodyParser . urlencoded ({ extended: true }));
-app.use(express.static('public'));
-app . listen ( 3000 );
